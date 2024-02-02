0948eac268d54ce63f34818489011e441f64de1a
in-deepaknegi <negideepak.in@gmail.com>
2024-02-02 18:18:13 +0530

    updated aside in [slug]/blog

---
+++ data/recents.js
@@ -0,0 +1,21 @@
+const recents = [
+    {
+        id: 1,
+        title:  "Enterprise design tips",
+        desc:  "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        href: "#"
+    },
+    {
+        id: 2,
+        title:  "We partnered up with Google",
+        desc:  "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        href: "#"
+    },
+    {
+        id: 3,
+        title:  "Our first project with React",
+        desc:  "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+        href: "#"
+    },
+]
+export default recents
