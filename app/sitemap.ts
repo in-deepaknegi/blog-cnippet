a7e0c9e2fcc03f8704fe29e23458ea5744e997dc
in-deepaknegi <negideepak.in@gmail.com>
2024-02-04 20:08:48 +0530

    SEO init

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://blog.cnippet.com',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://blog.cnippet.com/blogs',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+
+        {
+            url: 'https://blog.cnippet.com/build-a-blog-with-nextjs-and-mdx',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+    ]
+}
